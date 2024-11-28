98304790690c45334d8fbbc3912efd7ff8ffdfd4
elianortega <elian.mortega@gmail.com>
2024-11-27 22:20:23 -0600

    feat: add keymaps to cmp.mapping in lsp

--- lua/elianortega/lazy/lsp.lua
+++ lua/elianortega/lazy/lsp.lua
@@ -64,6 +64,8 @@ return {
         end,
       },
       mapping = cmp.mapping.preset.insert({
+        ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+        ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
         ["<C-b>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<C-Space>"] = cmp.mapping.complete(),
