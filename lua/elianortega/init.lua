a5df3c4ce0eabc44705bd226e6eca2ae373978f9
elianortega <elian.mortega@gmail.com>
2024-12-02 11:11:50 -0600

    feat: udpate lsp setup and setup flutter tools

--- lua/elianortega/init.lua
+++ lua/elianortega/init.lua
@@ -3,3 +3,18 @@ require("elianortega.lazy")
 require("elianortega.set")
 
 vim.cmd("let g:netrw_liststyle = 3")
+
+local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
+local yank_group = augroup("HighlightYank", {})
+
+autocmd("TextYankPost", {
+	group = yank_group,
+	pattern = "*",
+	callback = function()
+		vim.highlight.on_yank({
+			higroup = "IncSearch",
+			timeout = 40,
+		})
+	end,
+})
--- lua/elianortega/plugins/flutter-tools.lua
+++ lua/elianortega/plugins/flutter-tools.lua
@@ -3,25 +3,20 @@ return {
 	lazy = false,
 	dependencies = {
 		"nvim-lua/plenary.nvim",
+		"stevearc/dressing.nvim",
 	},
 	config = function()
 		require("flutter-tools").setup({
-			fvm = true,
-			decorations = {
-				statusline = {
-					-- set to true to be able use the 'flutter_tools_decorations.app_version' in your statusline
-					-- this will show the current version of the flutter app from the pubspec.yaml file
-					app_version = true,
-					-- set to true to be able use the 'flutter_tools_decorations.device' in your statusline
-					-- this will show the currently running device if an application was started with a specific
-					-- device
-					device = true,
-					-- set to true to be able use the 'flutter_tools_decorations.project_config' in your statusline
-					-- this will show the currently selected project configuration
-					project_config = true,
-				},
+			debugger = {
+				enabled = true,
+				register_configurations = function(_)
+					require("dap").configurations.dart = {}
+					require("dap.ext.vscode").load_launchjs()
+				end,
 			},
 		})
 		require("telescope").load_extension("flutter")
+		local keymap = vim.keymap -- for conciseness
+		keymap.set("n", "<leader>fc", "<cmd>Telescope flutter commands<cr>", { desc = "Show all flutter commands" })
 	end,
 }
