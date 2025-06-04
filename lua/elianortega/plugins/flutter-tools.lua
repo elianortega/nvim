291c97e917275fed4295d8c40987d3a77fb551c6
elianortega <elian.mortega@gmail.com>
2025-06-04 10:44:56 -0600

    chore: update avante

--- lua/elianortega/plugins/flutter-tools.lua
+++ lua/elianortega/plugins/flutter-tools.lua
@@ -15,64 +15,64 @@ return {
 			"stevearc/dressing.nvim",
 		},
 		config = function()
+			local flutter_tools = require("flutter-tools")
+			flutter_tools.setup({
+				fvm = true,
+				lsp = {
+					color = {
+						enabled = true,
+						background = false,
+						background_color = { r = 19, g = 17, b = 24 },
+						foreground = false,
+						virtual_text = true,
+						virtual_text_str = "■",
+					},
+					-- settings = {
+					-- 	analysisExcludedFolders = excluded_folders,
+					-- },
+				},
+				dev_log = {
+					enabled = true,
+					open_cmd = "tabedit",
+					filter = function(line)
+						if line:match("^D/EGL") then
+							return false
+						end
+						return true
+					end,
+				},
+				debugger = {
+					enabled = true,
+					register_configurations = function(_)
+						require("dap").configurations.dart = {}
+						require("dap.ext.vscode").load_launchjs()
+					end,
+				},
+			})
 			-- Get Flutter SDK path using 'which flutter' from fvm
-			local executable = require("flutter-tools.executable")
-			local path = require("plenary.path") -- Plenary's path helper
+			-- local executable = require("flutter-tools.executable")
+			-- local path = require("plenary.path") -- Plenary's path helper
 
 			-- Fetch the paths
-			executable.get(function(paths)
-				if not paths or not paths.flutter_sdk or not paths.dart_sdk then
-					print("Error: Unable to determine Flutter or Dart SDK paths.")
-					return
-				end
-
-				local flutter_sdk_path = paths.flutter_sdk
-
-				print(flutter_sdk_path)
-				-- Add custom paths to be excluded from analysis
-				local excluded_folders = {
-					vim.fn.expand("~/.pub-cache"), -- Default pub-cache
-					path:new(flutter_sdk_path, "packages"):absolute(), -- Flutter packages directory
-					path:new(flutter_sdk_path, ".pub-cache"):absolute(), -- Flutter .pub-cache
-				}
-
-				print(excluded_folders)
-
-				local flutter_tools = require("flutter-tools")
-				flutter_tools.setup({
-					fvm = true,
-					lsp = {
-						color = {
-							enabled = true,
-							background = false,
-							background_color = { r = 19, g = 17, b = 24 },
-							foreground = false,
-							virtual_text = true,
-							virtual_text_str = "■",
-						},
-						settings = {
-							analysisExcludedFolders = excluded_folders,
-						},
-					},
-					dev_log = {
-						enabled = true,
-						open_cmd = "tabedit",
-						filter = function(line)
-							if line:match("^D/EGL") then
-								return false
-							end
-							return true
-						end,
-					},
-					debugger = {
-						enabled = true,
-						register_configurations = function(_)
-							require("dap").configurations.dart = {}
-							require("dap.ext.vscode").load_launchjs()
-						end,
-					},
-				})
-			end)
+			-- executable.get(function(paths)
+			-- 	if not paths or not paths.flutter_sdk or not paths.dart_sdk then
+			-- 		print("Error: Unable to determine Flutter or Dart SDK paths.")
+			-- 		return
+			-- 	end
+			--
+			-- 	local flutter_sdk_path = paths.flutter_sdk
+			--
+			-- 	print(flutter_sdk_path)
+			-- 	-- Add custom paths to be excluded from analysis
+			-- 	local excluded_folders = {
+			-- 		vim.fn.expand("~/.pub-cache"), -- Default pub-cache
+			-- 		path:new(flutter_sdk_path, "packages"):absolute(), -- Flutter packages directory
+			-- 		path:new(flutter_sdk_path, ".pub-cache"):absolute(), -- Flutter .pub-cache
+			-- 	}
+			--
+			-- 	print(excluded_folders)
+			--
+			-- end)
 
 			require("dap").configurations.dart = {}
 			require("dap.ext.vscode").load_launchjs()
@@ -94,7 +94,7 @@ return {
 			end, { desc = "Wrap with Widget" })
 
 			-- Remove Widget
-			keymap.set("n", "<leader>wr", function()
+			keymap.set("n", "<leader>rw", function()
 				vim.lsp.buf.code_action({
 					title = "Remove Widget",
 					apply = true,
